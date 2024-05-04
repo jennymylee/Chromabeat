7fd3dd4055d25f849421dc0062963e32ea12c235
Jenny Lee <jennylee038@gmail.com>
2024-05-04 01:18:03 -0700

    record rotates

--- chromabeat/src/components/Record/Record.js
+++ chromabeat/src/components/Record/Record.js
@@ -1,28 +1,45 @@
 import "./Record.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheckSquare, faCoffee } from "@fortawesome/fontawesome-free-solid";
+import React, { useState } from "react";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import PauseIcon from "@mui/icons-material/Pause";
 
 export default function Record() {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const togglePlay = () => {
+    setIsPlaying(!isPlaying);
+  };
+
   return (
     <div className="Record">
       <div id="box">
         <div id="shadow">
-          <div id="vinyl">
+          <div
+            id="vinyl"
+            className={isPlaying ? "spin-animation" : ""}
+            onClick={togglePlay}
+          >
             <div id="inner">
               <div id="label">
-                <div id="hole"></div>
-                <span id="title">Rum Runner Sound System</span>
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/en/5/57/Laufey_-_Everything_I_Know_About_Love.png"
+                  alt="Album Cover"
+                />
               </div>
             </div>
           </div>
+          <button className="record-button" onClick={togglePlay}>
+            {isPlaying ? (
+              <PauseIcon sx={{ fontSize: "32px" }} />
+            ) : (
+              <PlayArrowIcon sx={{ fontSize: "32px" }} />
+            )}
+          </button>
         </div>
       </div>
       <div id="controls">
-        <button id="play">
-          <FontAwesomeIcon icon="coffee" />
-        </button>
-        <button id="stop">
-          <FontAwesomeIcon icon="fas fa-stop"></FontAwesomeIcon>
+        <button id="play" onClick={togglePlay}>
+          {isPlaying ? <PauseIcon /> : <PlayArrowIcon />}
         </button>
       </div>
     </div>
