23c1dce160fd8b21db69b6259cbfdb79dd532f5a
Jenny Lee <jennylee038@gmail.com>
2024-05-04 03:25:42 -0700

    songtitleview js

---
+++ chromabeat/src/components/SongTitleView/SongTitleView.js
@@ -0,0 +1,14 @@
+import "./SongTitleView.css";
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+
+export default function SongTitleView() {
+  return (
+    <div className="song-title-view">
+      <MusicNoteIcon sx={{ color: "white" }} />
+      <div className="text">
+        <p className="now-playing">Now playing...</p>
+        <p className="song-title">Trouble by Laufey</p>
+      </div>
+    </div>
+  );
+}
