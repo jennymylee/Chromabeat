a27442709e177f9e9f33c2341acb4af7992b71b4
Jenny Lee <jennylee038@gmail.com>
2024-05-08 06:31:18 -0500

    animation on a diff audio player

--- chromabeat/src/components/Animation/Animation.js
+++ chromabeat/src/components/Animation/Animation.js
@@ -6,30 +6,8 @@ export default function Animation(props) {
   const [analyser, setAnalyser] = useState(null);
   const [dataArray, setDataArray] = useState(null);
 
-  function handleCanvasClick() {
+  function handlePlay(event) {
     const audio1 = document.getElementById("audio1");
-    audio1.src = props.song;
-    const audioContext = new AudioContext();
-    audio1.play();
-    const source = audioContext.createMediaElementSource(audio1);
-    const analyzer = audioContext.createAnalyser();
-    source.connect(analyzer);
-    analyzer.connect(audioContext.destination);
-    analyzer.fftSize = 128;
-    const bufferLength = analyzer.frequencyBinCount;
-    const dataArray = new Uint8Array(bufferLength);
-    setAudioSource(source);
-    setAnalyser(analyzer);
-    setDataArray(dataArray);
-  }
-
-  function handleFileChange(event) {
-    const files = event.target.files;
-    const audio1 = document.getElementById("audio1");
-    audio1.src = URL.createObjectURL(files[0]);
-    audio1.load();
-    audio1.play();
-
     const audioContext = new AudioContext();
     const source = audioContext.createMediaElementSource(audio1);
     const analyzer = audioContext.createAnalyser();
@@ -42,6 +20,27 @@ export default function Animation(props) {
     setAnalyser(analyzer);
     setDataArray(dataArray);
   }
+
+  //   function handleFileChange(event) {
+  //     const files = event.target.files;
+  //     const audio1 = document.getElementById("audio1");
+  //     // audio1.src = URL.createObjectURL(files[0]);
+  //     audio1.src = props.song.src;
+  //     audio1.load();
+  //     audio1.play();
+
+  //     const audioContext = new AudioContext();
+  //     const source = audioContext.createMediaElementSource(audio1);
+  //     const analyzer = audioContext.createAnalyser();
+  //     source.connect(analyzer);
+  //     analyzer.connect(audioContext.destination);
+  //     analyzer.fftSize = 128;
+  //     const bufferLength = analyzer.frequencyBinCount;
+  //     const dataArray = new Uint8Array(bufferLength);
+  //     setAudioSource(source);
+  //     setAnalyser(analyzer);
+  //     setDataArray(dataArray);
+  //   }
 
   React.useEffect(() => {
     if (analyser) {
@@ -87,17 +86,19 @@ export default function Animation(props) {
 
   return (
     <div id="container">
-      <h1 onClick={handleCanvasClick}>
-        Click somewhere on canvas or choose local audio file to play!
-      </h1>
       <canvas id="canvas1"></canvas>
-      <audio id="audio1" controls></audio>
-      <input
+      <audio
+        id="audio1"
+        src={props.song.src}
+        controls
+        onPlay={handlePlay}
+      ></audio>
+      {/* <input
         type="file"
         id="fileupload"
         accept="audio/*"
         onChange={handleFileChange}
-      />
+      /> */}
     </div>
   );
 }
