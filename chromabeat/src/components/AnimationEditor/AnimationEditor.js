376cd70ea0729576d21da35d6ef61f9470b259b3
karen-phan <125514664+karen-phan@users.noreply.github.com>
2024-05-20 15:19:41 -0700

    open dial on click

--- chromabeat/src/components/AnimationEditor/AnimationEditor.js
+++ chromabeat/src/components/AnimationEditor/AnimationEditor.js
@@ -1,12 +1,45 @@
 import "./AnimationEditor.css";
 import React, { useState } from "react";
+import CloseIcon from "@mui/icons-material/Close";
 
 export default function AnimationEditor() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [visibleCircles, setVisibleCircles] = useState(false);
+  const [label, setLabel] = useState(null);
+
+  // click on circle 1
+  const handleFirstCircleClick = () => {
+    if (!visibleCircles) {
+      setVisibleCircles(true); // if circles aren't visible, clicking shows the circles
+    }
+    setLabel("circle 1"); // if circles are visible, clicking shows the label
+  };
+
+  // click on circles 2 & 3
+  const handleClick = (desc) => {
+    setLabel(desc);
+  };
+
+  // close the editor
+  const closeEditor = () => {
+    setIsOpen(!isOpen);
+    setLabel(null);
+  };
+
   return (
     <div className="animation-editor">
-      <div className="quarter-circle"></div>
-      <div className="quarter-circle"></div>
-      <div className="quarter-circle"></div>
+      {/* <button className="close-btn" onClick={closeEditor}>
+        <CloseIcon />
+      </button> */}
+      <div className="quarter-circle" onClick={handleFirstCircleClick}></div>
+      <div
+        className={`quarter-circle ${visibleCircles ? "true" : ""}`} // visible if true
+        onClick={() => handleClick("circle 2")}
+      ></div>
+      <div
+        className={`quarter-circle ${visibleCircles ? "true" : ""}`} // visible if true
+        onClick={() => handleClick("circle 3")}
+      ></div>
     </div>
   );
 }
