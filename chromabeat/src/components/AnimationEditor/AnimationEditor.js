c47a159acbbce7802071dcdfd7165c742e039e4f
karen-phan <125514664+karen-phan@users.noreply.github.com>
2024-05-27 16:31:26 -0700

    started animation controls knob

--- chromabeat/src/components/AnimationEditor/AnimationEditor.js
+++ chromabeat/src/components/AnimationEditor/AnimationEditor.js
@@ -1,45 +1,49 @@
 import "./AnimationEditor.css";
 import React, { useState } from "react";
-import CloseIcon from "@mui/icons-material/Close";
-import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
-import { IconButton } from "@mui/material";
+import Knob from "../Knob/Knob";
+// knob credit: https://codepen.io/bbx/pen/QBKYOy
 
 export default function AnimationEditor() {
-  const [visibleCircles, setVisibleCircles] = useState(false);
-  const [label, setLabel] = useState(null);
+  const [pointers, setPointers] = useState([{ value: 0 }, { value: 20 }]);
 
-  // click on the build or close icon
-  const handleIconClick = () => {
-    setVisibleCircles(!visibleCircles);
-  };
-
-  // click on circle to show label
-  const handleClick = (desc) => {
-    if (visibleCircles) {
-      setLabel(desc);
-    }
+  const handleKnobChange = (index, newValue) => {
+    const newPointers = [...pointers];
+    newPointers[index].value = newValue;
+    setPointers(newPointers);
   };
 
   return (
     <div className="animation-editor">
-      <div className="quarter-circle">
-        <IconButton className="icon-btn" size="large" onClick={handleIconClick}>
-          {visibleCircles ? <CloseIcon /> : <SettingsOutlinedIcon />}
-        </IconButton>
+      <div className="knob-container">
+        <div className="knob2">
+          <Knob
+            size={150}
+            numTicks={25}
+            degrees={260}
+            min={1}
+            max={100}
+            value={pointers[1].value}
+            color={"pink"}
+            outlineColor={"purple"}
+            hue={"330"}
+            onChange={(value) => handleKnobChange(1, value)}
+          />
+        </div>
+        <div className="knob1">
+          <Knob
+            size={80}
+            numTicks={25}
+            degrees={260}
+            min={1}
+            max={100}
+            value={pointers[0].value}
+            color={"yellow"}
+            outlineColor={"red"}
+            hue={"60"}
+            onChange={(value) => handleKnobChange(0, value)}
+          />
+        </div>
       </div>
-      {visibleCircles && (
-        <>
-          <div
-            className="quarter-circle true"
-            onClick={() => handleClick("circle 2")}
-          ></div>
-          <div
-            className="quarter-circle true"
-            onClick={() => handleClick("circle 3")}
-          ></div>
-        </>
-      )}
-      {label && <div className="label">{label}</div>}
     </div>
   );
 }
